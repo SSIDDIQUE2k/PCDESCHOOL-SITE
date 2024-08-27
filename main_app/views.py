e73c8aa139c84a4fb1d238c511de858572230491
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-08-26 22:59:47 -0400

    about page added

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 # main_app/views.py
 
 from django.shortcuts import render
-
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Peoples DayCare And Elementary School</h1>')
+    return HttpResponse('<h1>People\'s DayCare And Elementary School</h1>')
+
+def about(request):
+    return render(request, 'about.html')
