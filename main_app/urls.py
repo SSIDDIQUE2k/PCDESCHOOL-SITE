298e46d6dc016123405559e5a2af448cd7bc4df5
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-08-27 04:20:30 -0400

    edit_btn-completed

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('teachers/', views.teacher_index, name='teacher-index'),
     path('teachers/<int:teacher_id>/', views.teacher_detail, name='teacher-detail'),
     path('teachers/create/', views.TeacherCreate.as_view(), name='teacher-create'),
+    path('teachers/<int:pk>/update/', views.TeacherUpdate.as_view(), name='teacher-update'),
+    path('teachers/<int:pk>/delete/', views.TeacherDelete.as_view(), name='teacher-delete'),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import CreateView  # Import CreateView from django.views.generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Teacher  # Import the Teacher model
 
 
 class TeacherCreate(CreateView):
     model = Teacher
     fields = '__all__'
+
+class TeacherUpdate(UpdateView):
+    model = Teacher
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = '__all__'
+
+class TeacherDelete(DeleteView):
+    model = Teacher
+    success_url = '/cats/'
 
 
 # Define the home view function
@@ -26,5 +35,10 @@ def teacher_index(request):
 def teacher_detail(request, teacher_id):
     teacher = Teacher.objects.get(id=teacher_id)
     return render(request, 'teachers/detail.html', {'teacher': teacher})
+
+# Define the TeacherUpdate view
+
+
+
 
 
